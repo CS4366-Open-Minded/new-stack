96ecb12977652974498f4086dff8545b46dfa1cb
Antonio Bujanda <antonio.bujanda@ttu.edu>
2018-04-01 18:42:27 -0500

    Finished article model.

--- new-stack/app/models.py
+++ new-stack/app/models.py
@@ -5,6 +5,8 @@ Definition of models.
 from django.db import models
 from django.contrib.auth.models import User
 
+import requests
+
 STATE_CHOICES = (
     ('AL', 'Alabama'),
     ('AK', 'Alaska'),
@@ -67,12 +69,83 @@ class Profile(models.Model):
         help_text=('Designates if a user has confirmed his/her email.'),
     )
 
+class ArticleManager(models.Manager):
+ 
+   def create_article(self, sourceID, sourceName, authorName, title, description, url, urlToImage, publishedOn):
+        article = self.model(source_ID=sourceID, source_Name=sourceName, author_Name=authorName, title=title, description=description, url=url, url_To_Image=urlToImage, published_On=publishedOn)
+        if self.filter(title=title).count() == 0:
+            article.save(using=self._db)
+        return article
+
+   def request_articles(self):
+       """
+       Requests 20 articles from News API
+       """
+       response = requests.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=6894e635a38d4de3be2367635985676d").json()
+        
+       for i in range(0, 20):
+           id = response["articles"][i]["source"]["id"]
+           sourceName = response["articles"][i]["source"]["name"]
+           author = response["articles"][i]["author"]
+           title = response["articles"][i]["title"]
+           description =  response["articles"][i]["description"]
+           url = response["articles"][i]["url"]
+           urlImage = response["articles"][i]["urlToImage"]
+           publishedAt = response["articles"][i]["publishedAt"]
+
+           self.create_article(id, sourceName, author, title, description, url, urlImage, publishedAt)
+        
+
 class Article(models.Model):
     source_ID = models.CharField(max_length=200, null=True)
-    source_Name = models.CharField(max_length=200)
-    author_Name = models.CharField(max_length=200)
+    source_Name = models.CharField(max_length=200, null=True)
+    author_Name = models.CharField(max_length=200, null=True)
     title = models.CharField(max_length=200)
+    description = models.CharField(max_length=254, null=True)
     url = models.URLField(max_length=200)
     url_To_Image = models.URLField(max_length=200)
     published_On = models.CharField(max_length=200)
-    category = models.CharField(max_length=200, null=True)
+
+    objects = ArticleManager()
+
+    def get_sourceID(self):
+        """
+        Returns the source ID of the article.
+        """
+        return self.source_ID
+
+    def get_source_name(self):
+        """
+        Returns the source name of the article.
+        """
+        return self.source_Name
+
+    def get_author(self):
+        """
+        Returns the author name of the article.
+        """
+        return self.author_Name
+
+    def get_title(self):
+        """
+        Returns the title of the article.
+        """
+        return self.title
+
+    def get_url(self):
+        """
+        Returns the url of the article.
+        """
+        return self.url
+
+    def get_url_to_image(self):
+        """
+        Returns the url to the image of the article.
+        """
+        return self.url_To_Image
+
+    def get_publication_date(self):
+        """
+        Returns the publication date of the article.
+        """
+        return self.published_On
--- new-stack/app/views.py
+++ new-stack/app/views.py
@@ -19,12 +19,20 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_text
 import requests
 
+from app.models import Article
 from .forms import BootstrapRegistrationForm
 from .tokens import account_activation_token
 
 def home(request):
     """Renders the home page."""
     assert isinstance(request, HttpRequest)
+    if request.user.is_authenticated:
+        Article.objects.request_articles()
+        return render(
+            request,
+            'app/account.html',
+            )
+
     return render(
         request,
         'app/index.html',
@@ -33,50 +41,6 @@ def home(request):
             'year':datetime.now().year,
         }
     )
-
-def getData(): 
-    #Retrieve data for backend
-    response = requests.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=6894e635a38d4de3be2367635985676d")
-    response = response.json()
-
-    sourceName = []
-    author = []
-    title = []
-    description = []
-    url = []
-    urlImage = []
-    publishedAt = []
-
-
-    for i in range(0,19):
-        sourceName.append(response["articles"][i]["source"]["name"]) 
-        author.append(response["articles"][i]["author"]) 
-        title.append(response["articles"][i]["title"])
-        description.append(response["articles"][i]["description"])
-        url.append(response["articles"][i]["url"])
-        urlImage.append(response["articles"][i]["urlToImage"])
-        publishedAt.append(response["articles"][i]["publishedAt"])
-    
-    Article = {
-        'Source': sourceName,
-        'Author': author,
-        'Title': title,
-        'Description': description,
-        'Url': url,
-        'UrlImage': urlImage,
-        'PublishedAt': publishedAt,
-        'range': range(10)
-    }
-    return Article
-
-#def home(request):
-#    assert isinstance(request, HttpRequest)
-#    Article = getData()
-#    return render(
-#        request,
-#        'app/index2.html',
-#        Article
-#        )
 
 def signup(request):
     """Render the signup page."""
